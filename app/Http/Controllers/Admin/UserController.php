83ceec3e93700db04720e57084376f5b4fa26620
Marcelo Marques <egs.marcelo@gmail.com>
2020-02-18 15:21:46 -0300

    usuario altera modulo

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -111,8 +111,77 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-        echo "RECEBENDO....";
+        $user = User::find($id);
+        
+        if($user){
+            $data = $request->only([
+                'name',
+                'email',
+                'password',
+                'password_confirmation'
+            ]);
+            $validator = Validator::make([
+                'name' => $data['name'],
+                'email' => $data['email']
+            ], [
+                'name' => ['required', 'string', 'max:100'],
+                'email' => ['required', 'string', 'email', 'max:100']        
+            ]);
+
+            // 1. Alteração do nome
+            $user->name = $data['name'];
+
+            // 2. Alteração do email
+            // 2.1 Primeiro, varificamos se o email foi alterado
+            if($user->email != $data['email']){
+                
+                // 2.2 Verificamos se o nome email já existe.
+                $hasEmail = User::where('email', $data['email'])->get();
+                
+                // 2.3 Se não existir, nós alteramos.
+                if(count($hasEmail) === 0){
+                    $user->email = $data['email'];
+                } else {
+                    $validator->errors()->add('email', __('validation.unique',[
+                        'attribute' => 'email'
+                    ]));
+                }
+
+            }
+
+            // 3. Alteração da senha
+            // 3.1 Verifica se o usuário digitou senha
+            if(!empty($data['password'])) {
+                // 3.2 Verifica se a confirmação está OK
+                
+                if(strlen($data['password']) >= 4){
+                    if($data['password'] === $data['password_confirmation']){
+                        // 3.3 Altera a senha
+                        $user->password = Hash::make($data['password']);
+                    } else {
+                        $validator->errors()->add('password', __('validation.confirmed',[
+                            'attribute' => 'password'
+                        ]));
+                    }
+                } else {
+                    $validator->errors()->add('password', __('validation.min.string',[
+                        'attribute' => 'password',
+                        'min' => 3
+                    ]));
+                }                   
+            }
+            
+            if(count( $validator->errors() ) > 0) {
+                return redirect()->route('users.edit', [
+                    'user' => $id
+                ])->withErrors($validator);
+            }
+
+
+            $user->save();
+        }
+
+        return redirect()->route('users.index');
     }
 
     /**
