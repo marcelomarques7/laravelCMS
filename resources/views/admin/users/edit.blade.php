c49c72c665d59808216388fedc8efc1d2f707f8c
Marcelo Marques <egs.marcelo@gmail.com>
2020-02-18 10:23:22 -0300

    primeiro commit

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('adminlte::page')
+
+@section('title', 'Editar Usuário')
+
+@section('content_header')
+    <h1>Editar Usuário</h1>
+@endsection
+
+
+@section('content')
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            <h5>
+                <i class="icon fas fa-ban"></i>
+                Ocorreu um erro.
+            </h5>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+    <div class="card">
+        <div class="card-body">
+            <form class="form-horizontal" method="POST" action="{{route('users.update', ['user'=>$user->id])}}">
+                @method('PUT')
+                @csrf
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nome completo</label>
+                    <div class="col-sm-10">
+                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{$user->name}}">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">E-mail</label>
+                    <div class="col-sm-10">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{$user->email}}">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nova Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Senha Novamente</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10">
+                        <input type="submit" value="Salvar" class="btn btn-success">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    
+
+@endsection
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,45 @@
+@extends('adminlte::page')
+
+@section('title', 'Usuários')
+
+@section('content_header')
+    <h1>
+        Meus Usuários
+        <a href="{{ route('users.create') }}" class="btn btn-sm btn-success" style="float: right;">Novo Usuário</a>
+    </h1>
+@endsection
+
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>
+                            <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-sm btn-info">Editar</a>
+                            <a href="{{ route('users.destroy', ['user' => $user->id]) }}" class="btn btn-sm btn-danger">Excluir</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+{{ $users->links() }}
+
+@endsection
