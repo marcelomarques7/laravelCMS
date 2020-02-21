a1df06d6fbc1dd7fe508fa5ba5ec0ac382fbd265
Marcelo Marques <egs.marcelo@gmail.com>
2020-02-21 18:14:55 -0300

    até o front do dashboard

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -38,7 +38,7 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Corpo</label>
                     <div class="col-sm-10">
-                        <textarea name="body" class="form-control">{{$page->body}}</textarea>                        
+                        <textarea name="body" class="form-control bodyfield">{{$page->body}}</textarea>                        
                     </div>
                 </div>
                 
@@ -52,6 +52,23 @@
         </div>
     </div>
 
-    
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>
+    <script>
+
+        tinymce.init({
+            selector:'textarea.bodyfield',
+            height:300,
+            menubar:false,
+            plugins:['link', 'table', 'image', 'autoresize', 'lists'],
+            toolbar:'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | table | link image | bullist numlist',
+            content_css:[
+                '{{asset('assets/css/content.css')}}'
+            ],
+            images_upload_url:'{{route('imageupload')}}',
+            images_upload_credentials:true,
+            convert_urls:false
+        });
+
+    </script>   
 
 @endsection
